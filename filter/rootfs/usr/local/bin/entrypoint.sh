a9c8cd3d7bab1a0080928261e3d05e24cb4f307a
TopTuK <cydoor88@gmail.com>
2022-06-26 22:21:29 +0300

    add incl lists to maps.d folder

--- filter/rootfs/usr/local/bin/entrypoint.sh
+++ filter/rootfs/usr/local/bin/entrypoint.sh
@@ -19,14 +19,14 @@ dockerize \
   -template /etc/rspamd/local.d/worker-controller.inc.templ:/etc/rspamd/local.d/worker-controller.inc \
   -template /etc/rspamd/override.d/redis.conf.templ:/etc/rspamd/override.d/redis.conf \
   -template /etc/rspamd/local.d/classifier-bayes.conf.templ:/etc/rspamd/local.d/classifier-bayes.conf \
-  -template /etc/rspamd/local.d/local_bl_domain.inc.templ:/etc/rspamd/local.d/local_bl_domain.inc \
-  -template /etc/rspamd/local.d/local_bl_from.inc.templ:/etc/rspamd/local.d/local_bl_from.inc \
-  -template /etc/rspamd/local.d/local_bl_ip.inc.templ:/etc/rspamd/local.d/local_bl_ip.inc \
-  -template /etc/rspamd/local.d/local_bl_rcpt.inc.templ:/etc/rspamd/local.d/local_bl_rcpt.inc \
-  -template /etc/rspamd/local.d/local_wl_domain.inc.templ:/etc/rspamd/local.d/local_wl_domain.inc \
-  -template /etc/rspamd/local.d/local_wl_from.inc.templ:/etc/rspamd/local.d/local_wl_from.inc \
-  -template /etc/rspamd/local.d/local_wl_ip.inc.templ:/etc/rspamd/local.d/local_wl_ip.inc \
-  -template /etc/rspamd/local.d/local_wl_rcpt.inc.templ:/etc/rspamd/local.d/local_wl_rcpt.inc \
+  -template /etc/rspamd/local.d/local_bl_domain.inc.templ:/etc/rspamd/local.d/maps.d/local_bl_domain.inc \
+  -template /etc/rspamd/local.d/local_bl_from.inc.templ:/etc/rspamd/local.d/maps.d/local_bl_from.inc \
+  -template /etc/rspamd/local.d/local_bl_ip.inc.templ:/etc/rspamd/local.d/maps.d/local_bl_ip.inc \
+  -template /etc/rspamd/local.d/local_bl_rcpt.inc.templ:/etc/rspamd/local.d/maps.d/local_bl_rcpt.inc \
+  -template /etc/rspamd/local.d/local_wl_domain.inc.templ:/etc/rspamd/local.d/maps.d/local_wl_domain.inc \
+  -template /etc/rspamd/local.d/local_wl_from.inc.templ:/etc/rspamd/local.d/maps.d/local_wl_from.inc \
+  -template /etc/rspamd/local.d/local_wl_ip.inc.templ:/etc/rspamd/local.d/maps.d/local_wl_ip.inc \
+  -template /etc/rspamd/local.d/local_wl_rcpt.inc.templ:/etc/rspamd/local.d/maps.d/local_wl_rcpt.inc \
   ${FILTER_VIRUS_ARGS} \
   -timeout ${WAITSTART_TIMEOUT} \
   /usr/sbin/rspamd -c /etc/rspamd/rspamd.conf -f
